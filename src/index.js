ab85a7ba0d5e9f80849f1057ce7cfd19042ca1b8
MariaChumerina <masha.chumerina@gmail.com>
2020-07-01 11:21:35 +0500

    add option format and arguments

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ export default function genDiff() {
   program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format [type]', 'output format')
     .parse(process.argv);
 }
