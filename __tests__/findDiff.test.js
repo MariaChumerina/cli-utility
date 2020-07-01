ecef7a51e45112fd5eb00363d9ee7b744c40ec29
MariaChumerina <masha.chumerina@gmail.com>
2020-07-01 21:28:48 +0500

    moved fixtures to root refactor test findDiff

--- __tests__/findDiff.test.js
+++ __tests__/findDiff.test.js
@@ -2,16 +2,19 @@ import { expect, test } from '@jest/globals';
 import path from 'path';
 import findDiff from '../src/findDiff.js';
 
-const filePath1 = path.resolve('__tests__', 'fixtures', 'file1.json');
-const filePath2 = path.resolve('__tests__', 'fixtures', 'file2.json');
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
+const filePath1 = getFixturePath('file1.json');
+const filePath2 = getFixturePath('file2.json');
+
+const expectedDiff = '{\n'
+    + '    host: hexlet.io\n'
+    + '  + timeout: 20\n'
+    + '  - timeout: 50\n'
+    + '  - proxy: 123.234.53.22\n'
+    + '  - follow: false\n'
+    + '  + verbose: true\n'
+    + '}';
 
 test('findDiff', () => {
-  expect(findDiff(`${filePath1}`, `${filePath2}`)).toEqual('{\n'
-      + '    host: hexlet.io\n'
-      + '  + timeout: 20\n'
-      + '  - timeout: 50\n'
-      + '  - proxy: 123.234.53.22\n'
-      + '  - follow: false\n'
-      + '  + verbose: true\n'
-      + '}');
+  expect(findDiff(`${filePath1}`, `${filePath2}`)).toEqual(expectedDiff);
 });
