e3d6b369847064a550601490ad4a355c5c33f601
MariaChumerina <masha.chumerina@gmail.com>
2020-07-02 12:28:53 +0500

    fix linter error

--- __fixtures__/epectedDiff.js
+++ __fixtures__/epectedDiff.js
@@ -1,8 +1,10 @@
-export const expectedDiff = '{\n'
-    + '    host: hexlet.io\n'
-    + '  + timeout: 20\n'
-    + '  - timeout: 50\n'
-    + '  - proxy: 123.234.53.22\n'
-    + '  - follow: false\n'
-    + '  + verbose: true\n'
-    + '}';
+export default function getExpectedDiff() {
+  return  '{\n'
+      + '    host: hexlet.io\n'
+      + '  + timeout: 20\n'
+      + '  - timeout: 50\n'
+      + '  - proxy: 123.234.53.22\n'
+      + '  - follow: false\n'
+      + '  + verbose: true\n'
+      + '}';
+}
--- __tests__/findDiff.test.js
+++ __tests__/findDiff.test.js
@@ -1,13 +1,14 @@
 import { describe, expect, test } from '@jest/globals';
 import path from 'path';
 import findDiff from '../src/findDiff.js';
-import { expectedDiff } from '../__fixtures__/epectedDiff.js';
+import getExpectedDiff from '../__fixtures__/epectedDiff.js';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const [beforeJson, afterJson] = [getFixturePath('before.json'), getFixturePath('after.json')];
 const [beforeYaml, afterYaml] = [getFixturePath('before.yaml'), getFixturePath('after.yaml')];
 const [beforeIni, afterIni] = [getFixturePath('before.ini'), getFixturePath('after.ini')];
 
+const expectedDiff = getExpectedDiff();
 describe('findDiff', () => {
   test('should work ok', () => {
     expect(findDiff(`${beforeJson}`, `${afterJson}`)).toEqual(expectedDiff);
