488baf886e0359af0a0136f547c1925096170b57
MariaChumerina <masha.chumerina@gmail.com>
2020-07-05 15:26:46 +0500

    refactor

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,28 +10,32 @@ function formatValue(value) {
   }
 }
 
+function formatStr(node, fullKey) {
+  const { modified, beforeValue, afterValue } = node;
+  const formattedBeforeValue = formatValue(beforeValue);
+  const formattedAfterValue = formatValue(afterValue);
+
+  switch (modified) {
+    case 'changed':
+      return `Property '${fullKey}' was updated. From ${formattedBeforeValue} to ${formattedAfterValue}`;
+    case 'deleted':
+      return `Property '${fullKey}' was removed`;
+    case 'inserted':
+      return `Property '${fullKey}' was added with value: ${formattedAfterValue}`;
+    case 'unchanged':
+      return [];
+    default: throw new Error('Unknown property');
+  }
+}
+
 export default function formatPlain(tree) {
   const iter = (subtree, parent = null) => subtree.flatMap((node) => {
-    const {
-      key, modified, beforeValue, afterValue, children,
-    } = node;
+    const { key, modified, children } = node;
     const fullKey = parent ? `${parent}.${key}` : key;
-    const formattedBeforeValue = formatValue(beforeValue);
-    const formattedAfterValue = formatValue(afterValue);
     if (children.length > 0 && modified === 'changed') {
       return iter(children, fullKey);
     }
-    switch (modified) {
-      case 'changed':
-        return `Property '${fullKey}' was updated. From ${formattedBeforeValue} to ${formattedAfterValue}`;
-      case 'deleted':
-        return `Property '${fullKey}' was removed`;
-      case 'inserted':
-        return `Property '${fullKey}' was added with value: ${formattedAfterValue}`;
-      case 'unchanged':
-        return [];
-      default: throw new Error('Unknown property');
-    }
+    return formatStr(node, fullKey);
   });
   return iter(tree).join('\n');
 }
