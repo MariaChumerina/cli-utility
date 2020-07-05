0d9033b4229cc3c7463bddb8aa93ecbbee857172
MariaChumerina <masha.chumerina@gmail.com>
2020-07-05 15:33:12 +0500

    refactor tests

--- __tests__/findDiff.test.js
+++ __tests__/findDiff.test.js
@@ -2,16 +2,14 @@ import { describe, expect, test } from '@jest/globals';
 import path from 'path';
 import fs from 'fs';
 import genDifference from '../index.js';
-import getDiff from '../__fixtures__/epectedStylishDiff.js';
-import getPlainDiff from '../__fixtures__/expectedPlainDiff.js';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const [beforeJson, afterJson] = [getFixturePath('before.json'), getFixturePath('after.json')];
 const [beforeYaml, afterYaml] = [getFixturePath('before.yaml'), getFixturePath('after.yaml')];
 const [beforeIni, afterIni] = [getFixturePath('before.ini'), getFixturePath('after.ini')];
 
-const expectedStylishDiff = getDiff();
-const expectedPlainDiff = getPlainDiff();
+const expectedStylishDiff = fs.readFileSync('__fixtures__/expectedStylishDiff', 'utf-8');
+const expectedPlainDiff = fs.readFileSync('__fixtures__/expectedPlainDiff', 'utf-8');
 const expectedJsonDiff = fs.readFileSync('__fixtures__/expectedJsonDiff.json', 'utf-8');
 describe('findDiff', () => {
   test('should be a stylish format', () => {
