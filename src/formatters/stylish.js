e62c20619d7ac8608ee24e66e2e9afeef3e4d2f8
MariaChumerina <masha.chumerina@gmail.com>
2020-07-05 15:18:59 +0500

    refactor

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,28 +6,34 @@ function formatObj(obj, depth) {
       + `${value}\n${spaces}}`, '');
 }
 
+function formatStr(node, spaces, depth) {
+  const {
+    key, modified, beforeValue, afterValue,
+  } = node;
+  const formattedBeforeValue = typeof beforeValue === 'object' ? formatObj(beforeValue, depth + 1) : beforeValue;
+  const formattedAfterValue = typeof afterValue === 'object' ? formatObj(afterValue, depth + 1) : afterValue;
+  switch (modified) {
+    case 'unchanged':
+      return `${spaces}  ${key}: ${formattedBeforeValue}\n`;
+    case 'inserted':
+      return `${spaces}+ ${key}: ${formattedAfterValue}\n`;
+    case 'deleted':
+      return `${spaces}- ${key}: ${formattedBeforeValue}\n`;
+    case 'changed':
+      return `${spaces}- ${key}: ${formattedBeforeValue}\n${spaces}+ ${key}: ${formattedAfterValue}\n`;
+    default: throw new Error('error');
+  }
+}
+
 function formatTree(subTree, depth = 1) {
-  const formatted = subTree.flatMap(({
-    key, modified, beforeValue, afterValue, children,
-  }) => {
+  const formatted = subTree.flatMap((node) => {
+    const { key, children } = node;
     const spaces = ' '.repeat(depth * tabLength - tabLength / 2);
-    const formattedBeforeValue = typeof beforeValue === 'object' ? formatObj(beforeValue, depth + 1) : beforeValue;
-    const formattedAfterValue = typeof afterValue === 'object' ? formatObj(afterValue, depth + 1) : afterValue;
     if (children.length > 0) {
       const formattedChildren = formatTree(children, depth + 1);
       return `${spaces}  ${key}: ${formattedChildren}${spaces}  }\n`;
     }
-    switch (modified) {
-      case 'unchanged':
-        return `${spaces}  ${key}: ${formattedBeforeValue}\n`;
-      case 'inserted':
-        return `${spaces}+ ${key}: ${formattedAfterValue}\n`;
-      case 'deleted':
-        return `${spaces}- ${key}: ${formattedBeforeValue}\n`;
-      case 'changed':
-        return `${spaces}- ${key}: ${formattedBeforeValue}\n${spaces}+ ${key}: ${formattedAfterValue}\n`;
-      default: throw new Error('error');
-    }
+    return formatStr(node, spaces, depth);
   });
   const begin = '{\n';
   const result = [begin, ...formatted];
