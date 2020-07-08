9aa5b983d5c026a8f372b5d6e496b6914b45529c
MariaChumerina <masha.chumerina@gmail.com>
2020-07-08 11:07:31 +0500

    refactor buildTreeDiff.js: remove iter.js, add guard clauses

--- src/buildTreeDiff.js
+++ src/buildTreeDiff.js
@@ -1,27 +1,28 @@
 import _ from 'lodash';
 
 export default function buildTreeDiff(originalObj, modifiedObj) {
-  const iter = (first, second) => {
-    const keys = _.union(Object.keys(first), Object.keys(second));
-    return keys.flatMap((key) => {
-      const beforeValue = first[key];
-      const afterValue = second[key];
-      const hasFirstKey = _.has(first, key);
-      const hasSecondKey = _.has(second, key);
-      let modified;
-      if (hasFirstKey && hasSecondKey) {
-        modified = _.isEqual(beforeValue, afterValue) ? 'unchanged' : 'changed';
-      } else if (hasFirstKey && !hasSecondKey) modified = 'deleted';
-      else if (!hasFirstKey && hasSecondKey) modified = 'inserted';
+  const keys = _.union(Object.keys(originalObj), Object.keys(modifiedObj));
+  return keys.flatMap((key) => {
+    const beforeValue = originalObj[key];
+    const afterValue = modifiedObj[key];
+    const hasFirstKey = _.has(originalObj, key);
+    const hasSecondKey = _.has(modifiedObj, key);
+    if (hasFirstKey && hasSecondKey) {
+      const modified = _.isEqual(beforeValue, afterValue) ? 'unchanged' : 'changed';
+      const children = _.isObject(beforeValue) && _.isObject(afterValue)
+        ? [...buildTreeDiff(beforeValue, afterValue)] : [];
       return [{
-        modified,
-        key,
-        beforeValue,
-        afterValue,
-        children: typeof beforeValue === 'object' && typeof afterValue === 'object'
-          ? [...iter(beforeValue, afterValue)] : [],
+        modified, key, beforeValue, afterValue, children,
       }];
-    });
-  };
-  return iter(originalObj, modifiedObj);
+    } if (hasFirstKey && !hasSecondKey) {
+      return [{
+        modified: 'deleted', key, beforeValue, afterValue, children: [],
+      }];
+    } if (!hasFirstKey && hasSecondKey) {
+      return [{
+        modified: 'inserted', key, beforeValue, afterValue, children: [],
+      }];
+    }
+    return [];
+  });
 }
