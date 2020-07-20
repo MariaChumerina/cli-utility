3729f7e9ea54596c105e8aa4eadd084bd70604ba
MariaChumerina <masha.chumerina@gmail.com>
2020-07-20 13:28:58 +0500

    small edits: add function getExpected diff to test change second param at parse to format add format txt to expected files add jest: true to config eslint

--- __tests__/findDiff.test.js
+++ __tests__/findDiff.test.js
@@ -1,22 +1,19 @@
-import { describe, expect, test } from '@jest/globals';
 import path from 'path';
 import fs from 'fs';
 import genDiff from '../index.js';
 
 const getFixturePath = (filename) => path.join('__fixtures__', filename);
+const getExpectedDiff = (format) => fs.readFileSync(`__fixtures__/expected${format}.txt`, 'utf-8');
 const extensions = ['.json', '.yaml', '.ini'];
-const expectedStylishDiff = fs.readFileSync('__fixtures__/expectedStylishDiff', 'utf-8');
-const expectedPlainDiff = fs.readFileSync('__fixtures__/expectedPlainDiff', 'utf-8');
-const expectedJsonDiff = fs.readFileSync('__fixtures__/expectedJsonDiff', 'utf-8');
 
 describe('findDiff', () => {
   test('should work', () => {
     extensions.forEach((extension) => {
       const filename1 = getFixturePath(`before${extension}`);
       const filename2 = getFixturePath(`after${extension}`);
-      expect(genDiff(`${filename1}`, `${filename2}`)).toEqual(expectedStylishDiff);
-      expect(genDiff(`${filename1}`, `${filename2}`, 'plain')).toEqual(expectedPlainDiff);
-      expect(genDiff(`${filename1}`, `${filename2}`, 'json')).toEqual(expectedJsonDiff);
+      expect(genDiff(`${filename1}`, `${filename2}`)).toEqual(getExpectedDiff('Stylish'));
+      expect(genDiff(`${filename1}`, `${filename2}`, 'plain')).toEqual(getExpectedDiff('Plain'));
+      expect(genDiff(`${filename1}`, `${filename2}`, 'json')).toEqual(getExpectedDiff('Json'));
     });
   });
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,8 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-export default function parse(fileContents, extname) {
-  switch (extname) {
+export default function parse(fileContents, format) {
+  switch (format) {
     case 'json':
       return JSON.parse(fileContents);
     case 'yaml':
